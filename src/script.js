b402b9040ec2d157294ce6048f66d6498dfb4395
JOEL KIGGWE <joelkiggwe94@gmail.com>
2020-10-23 03:56:10 +0300

    Add logging functionality

--- src/script.js
+++ src/script.js
@@ -1,195 +1,91 @@
-// Blog Page
-//  window.addEventListener('load', () => {
-const category = document.querySelector(".category");
-const title = document.querySelector(".title");
-const date = document.querySelector(".date");
-const contentText = document.querySelector(".text");
+// Log in Page
 
-// articles
+// Obtain values on login
+const signInForm = document.querySelector(".form-signin");
 
-let articles = [
-  {
-    id: 1,
-    category: "Technology",
-    title: "Introduction to Web Development",
-    date: "01/10/20",
-    text: `In this course, you will learn the fundamentals of web development.`,
-  },
-  {
-    id: 2,
-    category: "Life",
-    title: "Work from Home",
-    date: "23/09/20",
-    text: "Are you suffering from a job loss? Here is my advice.",
-  },
-  {
-    id: 3,
-    category: "Life",
-    title: "Work from Home",
-    date: "23/09/20",
-    text: "Are you suffering from a job loss? Here is my advice.",
-  },
-  {
-    id: 4,
-    category: "Technology",
-    title: "Introduction to Web Development",
-    date: "01/10/20",
-    text: `In this course, you will learn the fundamentals of web development.`,
+const checkUser = () => {
+  const email = document.querySelector("#inputEmail").value;
+  const password = document.querySelector("#inputPassword").value;
+  const user = JSON.parse(localStorage.getItem("user"));
+  
+  // console.log("Submit Event");
+  if (user[0] === null) {
+    alert("Sign up please!");
+    location.reload();
   }
-];
 
-// List articles or none
-
-function displayArticles(articles) {
-  let articleDiv = document.querySelector(".article-section");
-  articleDiv.innerHTML = articles.map(
-    (article) => `<article class="blog">
-            <strong class="category">${article.category}</strong>
-            <h2 class="title">#<span id="id">${article.id}</span> - ${article.title}</h2>
-            <div class="date">${article.date}</div>
-            <p class="editable">${article.text}</p>
-            <a href="#" class="stretched-link">Read More...</a>
-            <div class="modify-btns">
-              <button class="btn btn-edit">Edit</button>
-              <button class="btn btn-delete">Delete</button>
-            </div>
-          </article>
-          `
-  )
-    .join("\n");
+  if (email === user[0].email && password === user[0].password) {
+    document.forms[0].action = '/blogPage.html';
+    alert(`Welcome to Blog Me! `);
+  } else {
+    alert("Sign up please!");
+  }
+  return;
 }
 
-displayArticles(articles);
-
-const addArticle = () => {
-  const addArticle = document.querySelector(".add-article");
-
-  // create form dynamically
-  addArticle.innerHTML = `<form class="new-article">
-   <label for="inputCategory">Category :</label>
-   <input type="text" id="inputCategory" class="form-control"
-   placeholder="Category" required autofocus>
-   <label for="inputTitle">Title :</label>
-   <textarea id="inputTitle" required></textarea>
-   <label for="inputDate">Date :</label>
-   <input type="date" id="inputDate"
-   class="form-control" placeholder="Date" required>
-   <label for="inputText">Text :</label>
-   <textarea id="inputText"></textarea>
-   <input type="submit" id="save" value="Save"/>
-   </form>`;
-
-  document.querySelector(".new-article").addEventListener("submit", (e) => {
-    e.preventDefault();
-    articles = readValues(articles);
-    displayArticles(articles);
-  });
-};
-
-document.querySelector(".btn-add").addEventListener("click", addArticle);
-
-// Obtain form values
-const readValues = (articles) => {
-  const category = document.querySelector("#inputCategory").value;
-  const date = document.querySelector("#inputDate").value;
-  const title = document.querySelector("#inputTitle").value;
-  const text = document.querySelector("#inputText").value;
-  articles.push({ category, date, title, text });
-  return articles;
-};
-
-const editBlog = (article) => {
-  let editCategory, editTitle, editDate, editText = '';
-
-  const blogItems = document.querySelector('.blog').children;
-
-  editCategory = blogItems[0].textContent;
-  editTitle = blogItems[1].textContent;
-  editDate = blogItems[2].textContent;
-  editText = blogItems[3].textContent;
-
-  editDate = new Date(editDate).toISOString().slice(0, 10);
-
-  // Create and insert a form for editing...
-
-  document.querySelector(".article-section").innerHTML =
-    `<form id="edit-text">    
-    <label for="inputCategory">Category :</label>
-    <input type="text" id="inputCategory" 
-    value=${editCategory} class="edit form-control" required/>
-    <label for="inputTitle">Title : </label>
-    <textarea class="edit form-control" id="inputTitle" required/>${editTitle}</textarea>
-    <label for="inputDate">Date:</label>
-    <input type="date" class="edit form-control" id="inputDate" 
-    value=${editDate} required/>
-    <label for="inputText">Text:</label>
-    <textarea class="edit">${editText}</textarea>  
-        <button type="submit" id="#save">Save</button>
-    </form>
-    `;
-
-  // Attach submit event listening on the newly placed form
-
-  document.querySelector("#edit-text").addEventListener("submit", (e) => {
-    e.preventDefault();
-    //read newly edited text
-    const editedCategory = document.querySelector("#inputCategory").value;
-    const editedTitle = document.querySelector("#inputTitle").value;
-    const editedDate = document.querySelector("#inputDate").value;
-    const editedText = document.querySelector(".edit").value;
-    const newArticle = { editedCategory, editedTitle, editedDate, editedText };
-    const newArticleInPlace = replaceWithEditedText(article, newArticle);
-        replaceAll(newArticleInPlace, articles);
-  });
-}
-
-const replaceWithEditedText = (article, newArticle) => {
-  article.text = newArticle.editedText;
-  article.category = newArticle.editedCategory;
-  article.title = newArticle.editedTitle;
-  article.date = newArticle.editedDate;
-  return article;
-};
-
-const replaceAll = (newArticle, articles) => {
-  document
-    .querySelector(".article-section")
-    .removeChild(document.querySelector("#edit-text"));
-let articleNumber = articles.findIndex( article => article === newArticle)
-    //replace the old article with the new one using an index from the original array     
-if( articleNumber !== -1) articles[articleNumber]= newArticle;
- displayArticles(articles);
-};
+// Submit, under login attempt
+signInForm.addEventListener("submit", () => {  
+  checkUser();
+});
 
 
-for (let elem of document.querySelectorAll(".blog")) {
-  (elem.querySelector(".btn-edit")).addEventListener("click", (e) => {
-    
-   let id =  +elem.querySelector('#id').textContent;
-    articleSelected =  articles.find( article => article.id === id );
-    editBlog(articleSelected);
-    e.stopPropagation();
-    
-  });
 
-}
-// Implement delete functionality
+//------------- Sign up form ----------------------
+// Create new user
 
-const deleteArticle = (elem, articles) => {
-  let final = articles.filter(art => {
-    return art.id !== +(elem.querySelector("#id").textContent);
-  });
+const section = document.querySelector(".forms");
+const signUpLink = document.querySelector(".link-sign-up");
+const signUpText = document.querySelector(".sign-up-text");
 
-  document.querySelector(".article-section").
-    removeChild(elem);
-    // console.log(final);
-}
+signUpLink.addEventListener('click', () => {
+  // change some elements, like intro text and button
 
-for (let elem of document.querySelectorAll(".blog")) {
-  (elem.querySelector(".btn-delete")).addEventListener("click", (e) => {
-    deleteArticle(elem, articles);
-    e.stopPropagation();
-    
-  });
+  document.querySelector(".intro").textContent = 'Create account...';
+  document.querySelector(".btn-sign-in").textContent = 'Sign up';
+  document.querySelector(".form-signin").removeChild(signUpText);
 
+  // change classList for the form
+  document.querySelector(".form-signin").classList.toggle("form-signin");
+  document.querySelector("form").classList.toggle("form-signup");
+  // document.querySelector("form");
+
+  // Obtain new class for the form
+  document.querySelector(".form-signup").addEventListener("submit",
+    (e) => {
+      e.preventDefault();
+      createUser();
+    });
+});
+
+
+// create a new user
+const createUser = () => {
+  const newEmailValue = document.querySelector("#inputEmail").value;
+  const newPasswordValue = document.querySelector("#inputPassword").value;
+
+
+  // create storage for the user
+  localStorage.setItem("user", JSON.stringify([]));
+  const user = JSON.parse(localStorage.getItem("user"));
+  let newUser = {
+    email: '', password: ''
+  };
+
+  if (user.length === 0) {
+    // insert new user and store
+    newUser.email = newEmailValue;
+    newUser.password = newPasswordValue;
+    user.push(newUser);
+    localStorage.setItem("user", JSON.stringify(user));
+    alert(`${newUser.email} is now registered!..`);
+    location.reload();
+
+
+  } else if (newEmailValue === user[0].email &&  // check registered
+    newPasswordValue === user[0].password
+    && user.length !== 0) {
+    alert("User already registered!..");
+    location.reload();
+  }
+  return;
 }
